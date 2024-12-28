c29341cf7a23caecfea62b5ad5986d57c3c56b78
asen-krasimirov <personalgrowth57@gmail.com>
2024-12-28 22:44:20 +0200

    [terraform] Fix eks.tf terraform file

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,7 +2,7 @@
 
 # Fetch the existing IAM role by its name
 data "aws_iam_role" "eks_role" {
-  name = var.aws_iam_role  # Replace with the name of your existing IAM role
+  name = var.aws_iam_user  # Replace with the name of your existing IAM role
 }
 
 # EKS Cluster
