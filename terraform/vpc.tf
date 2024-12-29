b89afbe25d44f54b5ceb0600add25448b6f23cd8
asen-krasimirov <personalgrowth57@gmail.com>
2024-12-29 14:10:55 +0200

    [terraform] ...

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,8 +10,33 @@ data "aws_internet_gateway" "existing_gw" {
   internet_gateway_id = var.internet_gateway_id  # Use the existing IGW ID
 }
 
-# Create Public Subnet
+# Check for the existing Public Subnet
+data "aws_subnet" "existing_public_subnet" {
+  filter {
+    name   = "cidr-block"
+    values = ["10.0.14.0/24"]  # CIDR block for the public subnet
+  }
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+# Check for the existing Private Subnet
+data "aws_subnet" "existing_private_subnet" {
+  filter {
+    name   = "cidr-block"
+    values = ["10.0.15.0/24"]  # CIDR block for the private subnet
+  }
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+# Create Public Subnet only if it doesn't already exist
 resource "aws_subnet" "public_subnet" {
+  count                   = length(data.aws_subnet.existing_public_subnet.id) == 0 ? 1 : 0
   vpc_id                  = data.aws_vpc.main1.id  # Reference the existing VPC
   cidr_block              = "10.0.14.0/24"  # Adjust this as needed
   availability_zone       = "${var.aws_region}a"
@@ -22,8 +47,9 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-# Create Private Subnet (for RDS)
+# Create Private Subnet only if it doesn't already exist
 resource "aws_subnet" "private_subnet" {
+  count                   = length(data.aws_subnet.existing_private_subnet.id) == 0 ? 1 : 0
   vpc_id                  = data.aws_vpc.main1.id  # Reference the existing VPC
   cidr_block              = "10.0.15.0/24"  # Private subnet CIDR block
   availability_zone       = "${var.aws_region}a"
