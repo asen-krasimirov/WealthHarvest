cd94356fe87aae739a1a8dcf7472358130080784
asen-krasimirov <personalgrowth57@gmail.com>
2024-12-29 09:35:09 +0200

    [terraform] Fix ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -31,13 +31,13 @@ resource "aws_ecr_repository" "backend_repo" {
   }
 }
 
-# Output the repository URIs
+# Output the repository URIs based on whether the repository was created or fetched
 output "auth_ecr_uri" {
   description = "The ECR URI for the auth repository"
-  value       = aws_ecr_repository.auth_repo[0].repository_url
+  value       = length(data.aws_ecr_repository.auth_repo.id) > 0 ? data.aws_ecr_repository.auth_repo.repository_url : aws_ecr_repository.auth_repo[0].repository_url
 }
 
 output "backend_ecr_uri" {
   description = "The ECR URI for the backend repository"
-  value       = aws_ecr_repository.backend_repo[0].repository_url
+  value       = length(data.aws_ecr_repository.backend_repo.id) > 0 ? data.aws_ecr_repository.backend_repo.repository_url : aws_ecr_repository.backend_repo[0].repository_url
 }
