2028b480e91cbc764f589ce96ccacd9754ab87d7
Асен Красимиров <68907559+asen-krasimirov@users.noreply.github.com>
2024-12-29 12:27:28 +0200

    Update frontend.tf [terraform]

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -1,5 +1,5 @@
 # Create the S3 bucket
-resource "aws_s3_bucket" "frontend_bucket_wealthharvest" {
+resource "aws_s3_bucket" "frontendbucketwealthharvest" {
   bucket = "my-frontend-bucket"
 
   website {
@@ -10,14 +10,14 @@ resource "aws_s3_bucket" "frontend_bucket_wealthharvest" {
 
 # Create the policy for the bucket
 resource "aws_s3_bucket_policy" "frontend_policy" {
-  bucket = aws_s3_bucket.frontend_bucket_wealthharvest.id
+  bucket = aws_s3_bucket.frontendbucketwealthharvest.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action    = "s3:GetObject"
         Effect    = "Allow"
-        Resource  = "${aws_s3_bucket.frontend_bucket_wealthharvest.arn}/*"
+        Resource  = "${aws_s3_bucket.frontendbucketwealthharvest.arn}/*"
         Principal = "*"
       }
     ]
@@ -26,6 +26,6 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
 
 # Set the ACL for the bucket
 resource "aws_s3_bucket_acl" "frontend_acl" {
-  bucket = aws_s3_bucket.frontend_bucket_wealthharvest.bucket
+  bucket = aws_s3_bucket.frontendbucketwealthharvest.bucket
   acl    = "public-read"
 }
