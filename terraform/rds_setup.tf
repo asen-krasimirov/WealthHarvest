15fe60253b1e052c910865158eccecd73c266d18
Асен Красимиров <68907559+asen-krasimirov@users.noreply.github.com>
2024-12-29 10:15:59 +0200

    [terraform] Update rds_setup.tf

--- terraform/rds_setup.tf
+++ terraform/rds_setup.tf
@@ -81,7 +81,7 @@ resource "aws_db_instance" "app_db_instance" {
   multi_az             = false
   storage_type         = "gp2"
   db_subnet_group_name = aws_db_subnet_group.default.name
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  vpc_security_group_ids = [aws_security_group.rds_sg[count.index].id]
 
   tags = {
     Name = "app-db-instance"
