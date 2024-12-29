283e6be7b285a312b054ff7d121eff27fb215229
asen-krasimirov <personalgrowth57@gmail.com>
2024-12-29 10:49:09 +0200

    [terraform] Fix frontend.tf

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -1,3 +1,10 @@
+resource "aws_s3_bucket_public_access_block" "frontend_bucket_access_block" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls = false
+  ignore_public_acls = false
+}
+
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = "my-frontend-bucket"
   acl    = "public-read"  # Allow public read access
@@ -6,13 +13,6 @@ resource "aws_s3_bucket" "frontend_bucket" {
     index_document = "index.html"
     error_document = "404.html"
   }
-}
-
-resource "aws_s3_bucket_public_access_block" "frontend_bucket_access_block" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-
-  block_public_acls = false
-  ignore_public_acls = false
 }
 
 resource "aws_s3_bucket_policy" "frontend_policy" {
